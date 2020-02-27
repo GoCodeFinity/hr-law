68ac646d102d13fa886bb827fea6c3200ee8318e
Manav Misra <manavm@visionwebsoft.com>
2020-02-27 11:15:40 -0600

    :speech_balloon: Add home page content.
    This is just using basic styling provided by Bulma classes.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,16 +1,33 @@
 <template>
   <Layout>
-    <!-- Author intro -->
-    <Author :show-title="true" />
+    <h1 class="title is-1 is-size-3-mobile">
+      Horas, Radice &amp; Associates
+    </h1>
 
-    <!-- List posts -->
-    <div class="posts">
-      <PostCard
-        v-for="edge in $page.posts.edges"
-        :key="edge.node.id"
-        :post="edge.node"
-      />
-    </div>
+    <p class="has-text-weight-bold">
+      At Horas, Radice &amp; Associates, we handle
+      <strong>serious injury and wrongful death cases,</strong> as well as
+      <strong>legal services for small and medium-sized businesses.</strong>
+    </p>
+
+    <h2 class="title is-2 is-size-4-mobile">Injury &amp; Wrongful Death</h2>
+    <h3 class="subtitle is-4 is-size-5-mobile">Robert J. Radice</h3>
+    <p>
+      Under the leadership of Robert J. Radice, our firm has over 35 years of
+      experience representing individuals that have been seriously injured, as
+      well as family members in wrongful death actions. Mr. Radice is
+      well-respected in the legal community, and has developed a reputation as a
+      thorough, dedicated and successful advocate.
+    </p>
+
+    <h2 class="title is-2 is-size-4-mobile">Business Law</h2>
+    <h3 class="subtitle is-4 is-size-5-mobile">John M. Horas</h3>
+    <p>
+      Under the leadership of John M. Horas, our firm has more than 30 years of
+      experience in representing small-to-medium sized businesses in every
+      aspect of their legal needs. Mr. Horas has developed a reputation in the
+      legal community as a detailed and pragmatic advocate.
+    </p>
   </Layout>
 </template>
 
