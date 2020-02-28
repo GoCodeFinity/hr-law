2c24c7c394662fa9f17281b1aa1e06bea87d22b6
Manav Misra <manavm@visionwebsoft.com>
2020-02-27 22:18:31 -0600

    :art: Toggle 2 columns on attorney pages for 'widescreen'

--- src/pages/BobRadice.vue
+++ src/pages/BobRadice.vue
@@ -4,63 +4,67 @@
       Robert "Bob" J. Radice
     </h1>
 
-    <section>
-      <h2 class="subtitle">About Mr. Radice</h2>
-      <figure class="image is-256x256 is-pulled-left">
-        <g-image
-          src="~/assets/images/bob-radice.jpg"
-          alt="Bronze skinned male with dark hair in a white shirt with striped red tie and a pinstripe dark colored suit."
-          width="350"
-        />
-      </figure>
-      <p>
-        Bob is a lifelong resident of St. Louis, attending St. Louis University
-        High School, as well as St. Louis University’s School of Law. He is
-        active in the St. Louis community, and is a member of the St. Louis
-        Soccer Hall of Fame. Bob graduated from SLU law in 1982 with Honors,
-        being named to the Order of the Woolsack for academic excellence.
-      </p>
+    <div class="columns is-widescreen">
+      <section class="column is-four-fifths">
+        <h2 class="subtitle">About Mr. Radice</h2>
+        <figure class="image is-256x256 is-pulled-left">
+          <g-image
+            src="~/assets/images/bob-radice.jpg"
+            alt="Bronze skinned male with dark hair in a white shirt with striped red tie and a pinstripe dark colored suit."
+            width="350"
+          />
+        </figure>
+        <p>
+          Bob is a lifelong resident of St. Louis, attending St. Louis
+          University High School, as well as St. Louis University’s School of
+          Law. He is active in the St. Louis community, and is a member of the
+          St. Louis Soccer Hall of Fame. Bob graduated from SLU law in 1982 with
+          Honors, being named to the Order of the Woolsack for academic
+          excellence.
+        </p>
 
-      <p>
-        Bob is licensed to practice law in the state and federal courts of both
-        Missouri and Illinois. He has achieved an AV rating as an attorney,
-        which is the highest rating that can be earned.
-      </p>
+        <p>
+          Bob is licensed to practice law in the state and federal courts of
+          both Missouri and Illinois. He has achieved an AV rating as an
+          attorney, which is the highest rating that can be earned.
+        </p>
 
-      <p>
-        Bob has tried over 100 cases to conclusion in both the state and federal
-        courts in Missouri and Illinois, with outstanding results. He has also
-        successfully argued appeals before the state and federal appeals courts.
-      </p>
+        <p>
+          Bob has tried over 100 cases to conclusion in both the state and
+          federal courts in Missouri and Illinois, with outstanding results. He
+          has also successfully argued appeals before the state and federal
+          appeals courts.
+        </p>
 
-      <p>
-        Bob has extensive experience in both the trial and settlement of
-        wrongful death cases, and serious personal injury matters, with trial
-        and settlement results totaling in the millions of dollars. Bob also has
-        handled many workers’ compensation claims on behalf of employees who
-        were seriously injured at work.
-      </p>
+        <p>
+          Bob has extensive experience in both the trial and settlement of
+          wrongful death cases, and serious personal injury matters, with trial
+          and settlement results totaling in the millions of dollars. Bob also
+          has handled many workers’ compensation claims on behalf of employees
+          who were seriously injured at work.
+        </p>
 
-      <p>
-        Bob’s cases are handled on a contingency fee basis, resulting in no fee
-        unless you receive compensation. In addition, consultations are free of
-        charge.
-      </p>
-    </section>
+        <p>
+          Bob’s cases are handled on a contingency fee basis, resulting in no
+          fee unless you receive compensation. In addition, consultations are
+          free of charge.
+        </p>
+      </section>
 
-    <section>
-      <h2 class="subtitle is-size-4-mobile">Practice Areas</h2>
-      <ul class="list has-background-grey-dark">
-        <li class="list-item">Personal Injury</li>
-        <li class="list-item">Wrongful Death</li>
-        <li class="list-item">Automobile & Truck Accidents</li>
-        <li class="list-item">Automobile Insurance Policy Claims</li>
-        <li class="list-item">Workers’ Compensation</li>
-        <li class="list-item">Premises Liability</li>
-        <li class="list-item">Medical Malpractice</li>
-        <li class="list-item">Products Liability</li>
-      </ul>
-    </section>
+      <section class="column">
+        <h2 class="subtitle is-size-4-mobile">Practice Areas</h2>
+        <ul class="list has-background-grey-dark">
+          <li class="list-item">Personal Injury</li>
+          <li class="list-item">Wrongful Death</li>
+          <li class="list-item">Automobile & Truck Accidents</li>
+          <li class="list-item">Automobile Insurance Policy Claims</li>
+          <li class="list-item">Workers’ Compensation</li>
+          <li class="list-item">Premises Liability</li>
+          <li class="list-item">Medical Malpractice</li>
+          <li class="list-item">Products Liability</li>
+        </ul>
+      </section>
+    </div>
   </Layout>
 </template>
 
--- src/pages/JackHoras.vue
+++ src/pages/JackHoras.vue
@@ -4,62 +4,64 @@
       John "Jack" M. Horas
     </h1>
 
-    <section>
-      <h2 class="subtitle">About Mr. Horas</h2>
-      <p>
-        Jack is a lifelong resident of St. Louis, attending Vianney High School
-        and is active in the St. Louis community. He attended the University of
-        Notre Dame Law where he received the A. Harold Weber Moot Court Award
-        Outstanding Achievement in Art of Oral Advocacy. He also attended Quincy
-        College, graduating magna cum laude, played on three National
-        Championship soccer teams, and was named to the Academic All-America
-        Soccer Team.
-      </p>
+    <div class="columns is-widescreen">
+      <section class="column is-four-fifths">
+        <h2 class="subtitle">About Mr. Horas</h2>
+        <p>
+          Jack is a lifelong resident of St. Louis, attending Vianney High
+          School and is active in the St. Louis community. He attended the
+          University of Notre Dame Law where he received the A. Harold Weber
+          Moot Court Award Outstanding Achievement in Art of Oral Advocacy. He
+          also attended Quincy College, graduating magna cum laude, played on
+          three National Championship soccer teams, and was named to the
+          Academic All-America Soccer Team.
+        </p>
 
-      <p>
-        Jack is licensed to practice law in the state and federal courts of both
-        Missouri and Illinois. He has achieved an AV rating as an attorney,
-        which is the highest rating that can be given.
-      </p>
+        <p>
+          Jack is licensed to practice law in the state and federal courts of
+          both Missouri and Illinois. He has achieved an AV rating as an
+          attorney, which is the highest rating that can be given.
+        </p>
 
-      <p>
-        Jack has been representing businesses for more than 30 years in every
-        aspect of their legal needs, including complex business litigation,
-        shareholder disputes, real estate transactions, general business advise,
-        and drafting and negotiating all types of business contracts and
-        agreements, including employment agreements, buy-sell agreements,
-        employee manuals, and shareholder and operating agreements. He also has
-        extensive experience in construction litigation, representing general
-        contractors, subcontractors, owners and sureties in litigation, mechanic
-        liens and subrogation matters.
-      </p>
+        <p>
+          Jack has been representing businesses for more than 30 years in every
+          aspect of their legal needs, including complex business litigation,
+          shareholder disputes, real estate transactions, general business
+          advise, and drafting and negotiating all types of business contracts
+          and agreements, including employment agreements, buy-sell agreements,
+          employee manuals, and shareholder and operating agreements. He also
+          has extensive experience in construction litigation, representing
+          general contractors, subcontractors, owners and sureties in
+          litigation, mechanic liens and subrogation matters.
+        </p>
 
-      <p>
-        Jack believes that his clients are best served when they can relate to
-        their attorney on a personal level. He uses his extensive experience to
-        provide common sense, practical, and effective legal representation for
-        his clients.
-      </p>
-    </section>
+        <p>
+          Jack believes that his clients are best served when they can relate to
+          their attorney on a personal level. He uses his extensive experience
+          to provide common sense, practical, and effective legal representation
+          for his clients.
+        </p>
+      </section>
 
-    <section>
-      <h2 class="subtitle">Practice Areas</h2>
-      <ul class="list has-background-grey-dark">
-        <li class="list-item">Commercial Litigation</li>
-        <li class="list-item">
-          Construction (Litigation, Contracts and Mechanic Liens)
-        </li>
-        <li class="list-item">Real Estate</li>
-        <li class="list-item">Shareholder Disputes</li>
-        <li class="list-item">
-          Business Organizations (including LLCs and Partnerships)
-        </li>
-        <li class="list-item">Business Acquisitions and Sales</li>
-        <li class="list-item">Commercial Transactions</li>
-        <li class="list-item">Subrogation</li>
-        <li class="list-item">Arbitration/Mediation</li>
-      </ul>
-    </section>
+      <section class="column">
+        <h2 class="subtitle">Practice Areas</h2>
+        <ul class="list has-background-grey-dark">
+          <li class="list-item">Commercial Litigation</li>
+          <li class="list-item">
+            Construction (Litigation, Contracts and Mechanic Liens)
+          </li>
+          <li class="list-item">Real Estate</li>
+          <li class="list-item">Shareholder Disputes</li>
+          <li class="list-item">
+            Business Organizations (including LLCs and Partnerships)
+          </li>
+          <li class="list-item">Business Acquisitions and Sales</li>
+          <li class="list-item">Commercial Transactions</li>
+          <li class="list-item">Subrogation</li>
+          <li class="list-item">Arbitration/Mediation</li>
+        </ul>
+      </section>
+    </div>
   </Layout>
 </template>
 
